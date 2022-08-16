87ceb41491f2aef7762f2c28d5834ea18bcda548
minhye9731 <jenney62639731@gmail.com>
2022-08-16 18:51:45 +0900

    Initial Commit

---
+++ TMBDFramework/TMBDFramework.h
@@ -0,0 +1,18 @@
+//
+//  TMBDFramework.h
+//  TMBDFramework
+//
+//  Created by 강민혜 on 8/16/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TMBDFramework.
+FOUNDATION_EXPORT double TMBDFrameworkVersionNumber;
+
+//! Project version string for TMBDFramework.
+FOUNDATION_EXPORT const unsigned char TMBDFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMBDFramework/PublicHeader.h>
+
+
