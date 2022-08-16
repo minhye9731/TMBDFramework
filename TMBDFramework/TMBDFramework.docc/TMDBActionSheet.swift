38ecbc0533ce8f748595c51cda190f0124f21baa
minhye9731 <jenney62639731@gmail.com>
2022-08-16 23:24:32 +0900

    alertaction

--- TMBDFramework/TMBDFramework.docc/TMDBActionSheet.swift
+++
@@ -1,8 +0,0 @@
-//
-//  TMDBActionSheet.swift
-//  
-//
-//  Created by 강민혜 on 8/16/22.
-//
-
-import Foundation
--- TMBDFramework/TMBDFramework.docc/TMDBAlert.swift
+++ TMBDFramework/TMBDFramework.docc/TMDBAlert.swift
@@ -5,4 +5,20 @@
 //  Created by 강민혜 on 8/16/22.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    public func showAlert(title: String, message: String, buttonTitle: String, buttonAction: @escaping((UIAlertAction) -> Void)) {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        let ok = UIAlertAction(title: buttonTitle, style: .default, handler: buttonAction)
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        
+        self.present(alert, animated: true)
+        
+    }
+}
